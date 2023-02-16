f1f06814c0c752b1f4dbbb9f978557f5e505d72a
Madelen Yoo <rolemadelen@tuta.io>
2023-02-16 11:44:51 +0900

    Card 3d hover effect
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- client/src/components/Picture/Picture.jsx
+++ client/src/components/Picture/Picture.jsx
@@ -2,9 +2,33 @@ import React, { memo } from 'react';
 import "./Picture.css";
 
 const Picture = memo((props) => {
+    const onMouseMoveHandler = (e) => {
+        const { clientX, clientY, currentTarget } = e;
+        const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
+        const card = currentTarget;        
+        const THRESHOLD = 15;
+
+        const horizontal = (clientX - offsetLeft) / clientWidth;
+        const vertical = (clientY - offsetTop) / clientHeight;
+
+        const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
+        const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
+
+        card.style.transform =`
+            perspective(${clientWidth}px) 
+            rotateX(${rotateY}deg) 
+            rotateY(${rotateX}deg) 
+            scale3d(1, 1, 1)
+        `;
+    }
+
+    const onMouseLeaveHandler = (e) => {
+        e.currentTarget.style.transform = `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
+    }
+
     return (
         <>
-            <div className="image">
+            <div className="image" onMouseMove={onMouseMoveHandler} onMouseLeave={onMouseLeaveHandler}>
                 <img src={`/src/assets/${props.imageName}`} alt={props.imageName}/>
             </div>
         </>
