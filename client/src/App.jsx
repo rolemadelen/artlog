e910d29cf78638c4599e8c0a963264429e8706b6
Madelen Yoo <rolemadelen@tuta.io>
2023-02-16 13:38:32 +0900

    client UI 어느정도 정리 완료
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,112 +5,153 @@ import Picture from './components/Picture/Picture'
 const dates = [
   {
     name: '2023.02.13',
+    detail: 'Hiratsuka, Kanagawa',
   },
   {
     name: '2023.02.12',
+    detail: 'Hiratsuka, Kanagawa',
   },
   {
     name: '2023.02.11',
+    detail: 'Hiratsuka Starbucks',
   },
   {
     name: '2023.02.09',
+    detail: 'Oshima',
   },
   {
     name: '2023.02.08',
+    detail: '히라츠카',
   },
   {
     name: '2023.02.07',
+    detail: '카나가와',
   },
   {
     name: '2023.02.06',
+    detail: 'Hiratsuka',
   },
   {
     name: '2023.02.04',
+    detail: 'Starbucks',
   },
   {
     name: '2023.02.03',
+    detail: '스타벅스',
   },
   {
     name: '2023.02.02',
+    detail: 'Kanagawa',
   },
   {
     name: '2023.02.01',
+    detail: 'Minatomirai',
   },
   {
     name: '2022.12.08',
+    detail: 'Oshima',
   },
   {
     name: '2022.12.09',
+    detail: 'Village House',
   },
   {
     name: '2022.12.10',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.11',
+    detail: 'Lorep Ipsum',
   },
   {
     name: '2022.12.12',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.13',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.14',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.15',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.16',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.17',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.18',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.19',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.20',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.21',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.22',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.23',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.24',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.25',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.26',
+    detail: 'Hiratsuka',
   },
   {
     name: '2022.12.27',
+    detail: 'Starbucks',
   },
   {
     name: '2022.12.28',
+    detail: 'Oshima',
   },
 ]
 
 const App = memo(() => {
-  const [imageName, setImageName] = useState('');
+  const [imageName, setImageName] = useState('2023.02.13.png');
+  const [imageDetail, setImageDetail] = useState(''); 
   const [dateIndex, setDateIndex] = useState(0);
-
+  
   useEffect(() => {
-    console.log("App Component useEffect");
+    console.debug('use effect');
+
     const x = document.querySelector(`li[name="${dates[dateIndex].name}"]`);
     x.classList.add('active');
-    x.scrollIntoView();
-    // setImageName(dates[dateIndex].name + '.png');
+    setImageName(dates[dateIndex].name + '.png');
+    setImageDetail(dates[dateIndex].detail);
+    x.scrollIntoView({
+      behavior: 'auto',
+      block: 'center',
+      inline: 'center'
+  });
+
+    return () => {};
   }, [dateIndex])
 
   const onWheelHandler = (event) => {
@@ -124,19 +165,26 @@ const App = memo(() => {
       index -= 1;
       x.classList.remove('active');
     } else {
-      if(dateIndex === dates.length - 1) return;
+      if(index === dates.length - 1) return;
       index += 1;
       x.classList.remove('active');
     }
     
+    console.debug(`onWheel dateIndex: `, dateIndex);
+    console.debug(`onWheel index: `, index);
+    console.debug(`onWheel x: `, x);
+
     x = document.querySelector(`li[name="${dates[index].name}"]`);
     x.classList.add('active');
     setDateIndex(index);
   }
 
-  const onTransitionEndHandler = (e) => {
-    console.log(e);
-    setImageName(dates[dateIndex].name + '.png');
+  const onClickHandler = (e) => {
+    const i = parseInt(e.currentTarget.dataset.index);
+    if(dateIndex === i) return;
+    
+    document.querySelector(`li[name="${dates[dateIndex].name}"]`).classList.remove('active');
+    setDateIndex(i);
   }
 
   return (
@@ -149,12 +197,12 @@ const App = memo(() => {
       </p>
     </aside>
     <main className="main-frame">
-      <Picture imageName={imageName}/>
+      <Picture imageName={imageName} imageDetail={imageDetail}/>
     </main>
-    <aside className="aside-right" onWheel={onWheelHandler} onTransitionEnd={onTransitionEndHandler}>
+    <aside className="aside-right" onWheel={onWheelHandler}>
       <ul>
         {dates.map((image, i) => (
-          <li key={image + i} name={image.name}>{image.name}</li>
+          <li key={image + i} name={image.name} data-index={i} onClick={onClickHandler}>{image.name}</li>
         ))}
       </ul>
     </aside>
