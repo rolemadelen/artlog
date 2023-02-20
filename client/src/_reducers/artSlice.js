9de21b8fb19c65715f6a187722e1f4dd60a9454f
rolemadelen <rolemadelen17@pm.me>
2023-02-20 15:00:50 +0900

    image saved and fetched from mongodb - base64 encoded

--- client/src/_reducers/artSlice.js
+++ client/src/_reducers/artSlice.js
@@ -6,16 +6,21 @@ export const artSlice = createSlice({
         name: '',
         note: '',
         cupless: undefined,
+        base64img: ''
     },
     reducers: {
         update: (state, action) => {
             state.name = action.payload.name + '.png';
             state.note = action.payload.note;
             state.cupless = action.payload.cupless;
+            state.base64img = action.payload.base64img;
         },
+        storeBase64Image: (state, action) => {
+            state.base64img = action.payload.base64img;
+        }
     }
 })
 
-export const { update } = artSlice.actions;
+export const { update, storeBase64Image } = artSlice.actions;
 
 export default artSlice.reducer;
