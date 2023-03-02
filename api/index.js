48b31780c020aafc407b9bb1f58410059aebef3b
Madelen Yoo <rolemadelen@tuta.io>
2023-03-02 18:35:26 +0900

    Revert "get changed to post"
    This reverts commit d9bb6f7d606d2133e20e4e3473dcd21b8acc8f4e.
    
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true
 });
 
-app.post('/api/artslist', (req, res) => {
+app.get('/api/artslist', (req, res) => {
     Art.find({}, (err, arts) => {
         if(err) return res.status(400).json({success: false, err});
         
@@ -33,10 +33,7 @@ app.post('/api/artslist', (req, res) => {
             else return -1;
         })
 
-        res.status(200).json({
-            success: true,
-            arts: artsMap,
-        })
+        res.status(200).send(artsMap);
     })
 })
 
